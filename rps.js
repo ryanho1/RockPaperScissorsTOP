7234f9ec0b41fa66b14931ffdf42f63b0953b78e
ryanho1 <ryan81645@gmail.com>
2024-08-17 02:16:18 +0800

    Completed step 2, will complete prompt function by tomorrow

--- rps.js
+++ rps.js
@@ -1,1 +1,22 @@
-console.log("helo world!");
+  
+    
+    function getComputerChoice() {
+    Math.floor(Math.random() * 4);
+        if (1){
+            console.log("rock");
+        } else if (2){
+            console.log('scissors');
+        }   else if (3){
+            console.log('paper');
+        }
+    }
+
+    function getHumanChoice(){
+        window.prompt("Pick rock, paper, or scissors","tf is this?")
+            // if (getComputerChoice = 1 || getHumanChoice = 3){
+            //     console.log("You win");
+            }
+    // }
+
+    console.log(getComputerChoice());
+    

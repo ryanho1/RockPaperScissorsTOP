22a30d5ddc86ea80c8e09cd680e1dc45f1dec598
ryanho1 <ryan81645@gmail.com>
2024-08-17 16:20:34 +0800

    im getting cooked, confused on why my && is not working

--- rps.js
+++ rps.js
@@ -1,22 +1,76 @@
   
     
     function getComputerChoice() {
-    Math.floor(Math.random() * 4);
-        if (1){
+    return Math.floor(Math.random() * 3 + 1);}
+
+    let compChoice = getComputerChoice();
+
+    function calculation()  {
+        if (compChoice === 1) {
             console.log("rock");
-        } else if (2){
+        } 
+        else if (compChoice === 2) {
             console.log('scissors');
-        }   else if (3){
+        }   
+        else if (compChoice === 3) {
             console.log('paper');
-        }
+        };
     }
 
-    function getHumanChoice(){
-        window.prompt("Pick rock, paper, or scissors","tf is this?")
-            // if (getComputerChoice = 1 || getHumanChoice = 3){
-            //     console.log("You win");
-            }
-    // }
+    console.log(calculation());
 
-    console.log(getComputerChoice());
+    function getHumanChoice(){
+            console.log(getComputerChoice());
+                let answer = window.prompt("choose rock paper or scissors");
+                if (getComputerChoice() === 1 && answer === "rock"){
+                    console.log('tie!');
+                } else if (getComputerChoice() === 2 && answer === "rock"){
+                    console.log('you win (rock kills scissors)');
+                } else if (getComputerChoice() === 3 && answer === "rock"){
+                    console.log('you lose (paper wraps rock!)');}
+                else if (getComputerChoice() === 1 && answer === "scissors"){
+                    console.log('you lose (scissors dies to rock)');
+                } else if (getComputerChoice() === 2 && answer === "scissors"){
+                    console.log('tie!');}
+                    else if (getComputerChoice() === 3 && answer === "scissors"){
+                    console.log('Scissors win (cut cut)');
+                } else if (getComputerChoice() === 1 && answer === "paper"){
+                    console.log('you win! (paper wraps around)');}
+                    else if (getComputerChoice() === 2 && answer === "paper"){
+                    console.log('you lose (got wrapped)');
+                } else if (getComputerChoice() === 3 && answer === "paper"){
+                    console.log('DRAW');}
+                    else {
+                        console.log('no match');
+                    }}
+            //      else if ("rock" && compChoice === 2){
+            //         console.log('you win');
+            //     } else if ("rock" && compChoice === 3){
+            //         console.log('you lose');
+            //     } else if ("scissors" && compChoice === 1){
+            //         console.log('you lose');
+            //     } else if ("scissors" && compChoice === 2){
+            //         console.log('tie!')
+            //     } else if ("scissors" && compChoice === 3){
+            //         console.log('you win');
+            //     } else if ("paper" && compChoice === 1){
+            //         console.log('you win');
+            //     } else if ("paper" && compChoice === 2){
+            //         console.log('you lose');
+            //     } else if ("paper" && compChoice === 3){
+            //         console.log('tie!');
+            //     } else if ('undefined') {
+            //         console.log('nig')
+            //     };
+            // }
+        
+
+    // console.log(getComputerChoice());
+    // console.log(calculation());
+    console.log(getHumanChoice()); 
+    console.log( typeof compChoice);
+    console.log( typeof getComputerChoice());
+    // console.log(typeof rock);
+
+    
     

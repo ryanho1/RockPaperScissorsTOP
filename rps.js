114ea119164b3607fa45df573f6b298b95950e9a
ryanho1 <ryan81645@gmail.com>
2024-08-18 22:06:59 +0800

    Added replayed rounds, figuring out how to make the computer choice different now

--- rps.js
+++ rps.js
@@ -3,7 +3,7 @@
     function getComputerChoice() {
     return Math.floor(Math.random() * 3 + 1);};
 
-    const ComputerChoice = getComputerChoice();
+    let ComputerChoice = getComputerChoice();
 
     let answer = prompt("choose rock paper or scissors");
 
@@ -12,34 +12,48 @@
     }
 
     let humanChoice = getHumanChoice();
-    let humanScore = 0;
-    let computerScore = 0;
-    let OverallScore
 
-    function playRound(humanChoice, computerChoice) {
-        if ((ComputerChoice === 1 && answer.toLowerCase() === "rock") || (ComputerChoice === 2 && answer.toLowerCase() === "scissors") || (ComputerChoice === 3 && answer.toLowerCase() === "paper")){
+    function playGame() {
+        let humanScore = 0;
+        let computerScore = 0;
+         let counter = playRound();
+         let roundsPlayed = counter;
+        function playRound() {
+                if ((ComputerChoice === 1 && answer.toLowerCase() === "rock") || (ComputerChoice === 2 && answer.toLowerCase() === "scissors") || (ComputerChoice === 3 && answer.toLowerCase() === "paper")){
             console.log('tie!');
         } 
         else if ((ComputerChoice === 2 && answer.toLowerCase() === "rock") || (ComputerChoice === 3 && answer.toLowerCase() === "scissors") || (ComputerChoice === 1 && answer.toLowerCase() === "paper")){
             console.log('you win') ;
             ++humanScore;
+            console.log(humanScore + "-" + computerScore);
         } 
         else if ((ComputerChoice === 3 && answer.toLowerCase() === "rock") || (ComputerChoice === 1 && answer.toLowerCase() === "scissors") || (ComputerChoice === 2 && answer.toLowerCase() === "paper")){
             console.log('you lose') ;
             ++computerScore;
+            console.log(humanScore + "-" + computerScore);
         }
         else {
             return 'you either inputted the wrong answer or its broken';
         }
-              } // test computer Score & HumanScore
+              } 
+            (humanScore + "-" + computerScore);
+   
+            let rrr = 1;
+            // let totalScore = "humanScore" + "computerScore";
+            for (let counter = 0 ;counter < 4; counter++) {
+                answer = prompt("choose rock paper or scissors round " + rrr++).toLowerCase();
+                console.log(playRound(ComputerChoice, humanChoice));
+                        }
+            }
+
 
 
     // console.log(getComputerChoice());
     // console.log( typeof getComputerChoice());
     // console.log(answer);
-    console.log(playRound());
-    console.log(humanScore);
-    console.log(computerScore);
+    console.log(playGame());
+    // console.log(humanScore + "-" + computerScore);
+
 
 
     

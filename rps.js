0664ddd17a19f835ccdc4d418f0ce470e2f176f5
ryanho1 <ryan81645@gmail.com>
2024-08-18 19:05:23 +0800

    Completed the playround code before I even stumbled across it, making me feel super smart. Nearly done with this.

--- rps.js
+++ rps.js
@@ -5,42 +5,37 @@
 
     const ComputerChoice = getComputerChoice();
 
-    let humanScore = 0;
-    let computerScore = 0;
     let answer = prompt("choose rock paper or scissors");
 
     function getHumanChoice(){
-            console.log(answer);
+        console.log(answer);
     }
-        
+
+    let humanChoice = getHumanChoice();
+    let humanScore = 0;
+    let computerScore = 0;
+    let OverallScore
+
     function playRound(humanChoice, computerChoice) {
-                if ((ComputerChoice === 1 && answer === "rock") || (ComputerChoice === 2 && answer === "scissors") || (ComputerChoice === 3 && answer === "paper")){
-                    console.log('tie!');
-                } 
-                else if ((ComputerChoice === 2 && answer === "rock") || (ComputerChoice === 3 && answer === "scissors") || (ComputerChoice === 1 && answer === "paper")){
-                    console.log('you win');
-                } 
-                else if ((ComputerChoice === 3 && answer === "rock") || (ComputerChoice === 1 && answer === "scissors") || (ComputerChoice === 2 && answer === "paper")){
-                    console.log('you lose');
-                }
-                else {
-                    return 'you either inputted the wrong answer or its broken';
-                }
+        if ((ComputerChoice === 1 && answer.toLowerCase() === "rock") || (ComputerChoice === 2 && answer.toLowerCase() === "scissors") || (ComputerChoice === 3 && answer.toLowerCase() === "paper")){
+            console.log('tie!');
+        } 
+        else if ((ComputerChoice === 2 && answer.toLowerCase() === "rock") || (ComputerChoice === 3 && answer.toLowerCase() === "scissors") || (ComputerChoice === 1 && answer.toLowerCase() === "paper")){
+            console.log('you win') && ++humanScore;
+        } 
+        else if ((ComputerChoice === 3 && answer.toLowerCase() === "rock") || (ComputerChoice === 1 && answer.toLowerCase() === "scissors") || (ComputerChoice === 2 && answer.toLowerCase() === "paper")){
+            console.log('you lose') && ++computerScore;
+        }
+        else {
+            return 'you either inputted the wrong answer or its broken';
+        }
               }
-              
-    const humanSelection = getHumanChoice();
-    // const ComputerChoice = getComputerChoice();
-              
-    playRound(humanSelection, ComputerChoice);
-        
 
     // console.log(getComputerChoice());
-    // console.log(calculation());
-    console.log(getHumanChoice()); 
     // console.log( typeof getComputerChoice());
-    console.log(answer);
+    // console.log(answer);
+    console.log (playRound());
 
-    // console.log(typeof rock);
 
     
     

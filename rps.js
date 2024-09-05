61649fab863c3b0d0f2a8416e92487c0e9063ff5
ryanho1 <ryan81645@gmail.com>
2024-09-05 14:26:09 +0800

    added DOM manipulation to RPS + cleaned up code

--- rps.js
+++ rps.js
@@ -1,51 +1,80 @@
-  
-    let answer = prompt("choose rock paper or scissors");
+    const rock = document.getElementById("rock");
+    const paper = document.getElementById("paper");
+    const scissors = document.getElementById("scissors");
+    let answer = "";
 
-    function getHumanChoice(){
-        console.log(answer);
-    }
+    rock.style.color = "blue";
+    paper.style.color = "red";
+    scissors.style.color = "green";
 
-    let humanChoice = getHumanChoice();
+    rock.addEventListener('click', function() {
+        answer = "rock";
+    });
+    paper.addEventListener('click', function() {
+        answer = "paper";
+    });
+    scissors.addEventListener('click', function() {
+        answer = "scissors";
+    });
 
-    function playGame() {
+    const result = document.querySelector("#result");
+    result.textContent = "Result: ";
+
+    const score = document.querySelector("#score");
+    score.textContent = "Score: ";
+
+    const finalScore = document.querySelector("#finalScore");
+    
+
+    function getComputerChoice() {
+    return Math.floor(Math.random() * 3 + 1);
+        };
         let humanScore = 0;
         let computerScore = 0;
-         let counter = playRound();
-         let roundsPlayed = counter;
+
+        function reset() {
+        humanScore = 0;
+        computerScore = 0;
+        finalScore.textContent = "Final Scoreboard: " + humanScore + " - " + computerScore;
+        score.textContent = ("Current scoreboard: 0-0");
+        }
+
         function playRound() {
-            function getComputerChoice() {
-                return Math.floor(Math.random() * 3 + 1);
-                };
-                var ComputerChoice = getComputerChoice();
-                if ((ComputerChoice === 1 && answer.toLowerCase() === "rock") || (ComputerChoice === 2 && answer.toLowerCase() === "scissors") || (ComputerChoice === 3 && answer.toLowerCase() === "paper")){
-            console.log('tie!');
-        } 
-        else if ((ComputerChoice === 2 && answer.toLowerCase() === "rock") || (ComputerChoice === 3 && answer.toLowerCase() === "scissors") || (ComputerChoice === 1 && answer.toLowerCase() === "paper")){
-            console.log('you win') ;
-            ++humanScore;
-            console.log(humanScore + "-" + computerScore);
-        } 
-        else if ((ComputerChoice === 3 && answer.toLowerCase() === "rock") || (ComputerChoice === 1 && answer.toLowerCase() === "scissors") || (ComputerChoice === 2 && answer.toLowerCase() === "paper")){
-            console.log('you lose') ;
-            ++computerScore;
-            console.log(humanScore + "-" + computerScore);
-        }
-        else {
-            return 'you either inputted the wrong answer or its broken';
-        }
-              } 
-                    
-              let rrr = 1;
-              for (let counter = 1 ;counter <= 4; counter++) {
-                answer = prompt("choose rock paper or scissors round " + rrr++).toLowerCase();
-                playRound();
-                        }
-                alert("final score: " + (humanScore + "-" + computerScore));
-                        
+        var ComputerChoice = getComputerChoice();
+        console.log (ComputerChoice);
+        if ((ComputerChoice === 1 && answer === 'rock') || (ComputerChoice === 2 && answer === "scissors") ||
+            (ComputerChoice === 3 && answer === 'paper'))
+            {
+            result.textContent = "Result: DRAW";
+            score.textContent = ("Current scoreboard: 0-0");
+            } 
+        else if ((ComputerChoice === 2 && answer === 'rock') || (ComputerChoice === 3 && answer === "scissors") ||
+            (ComputerChoice === 1 && answer === 'paper'))
+            {
+            result.textContent = "Result: You WIN"; 
+            score.textContent = ("Current scoreboard: 1-0");
+            humanScore++;
+            } 
+        else if ((ComputerChoice === 3 && answer === 'rock') || (ComputerChoice === 1 && answer === "scissors") || 
+            (ComputerChoice === 2 && answer === 'paper'))
+            {
+            result.textContent = "Result: You LOSE";
+            score.textContent = ("Current scoreboard: 0-1");
+            computerScore++;
             }
-    console.log(playGame());
+        else {
+            return "no";
+        }
+        finalScore.textContent = "Final Scoreboard: " + humanScore + " - " + computerScore;
+
+        if (humanScore === 5){
+            alert ("human wins");
+        } else if (computerScore === 5){
+            alert ("you lose");
+        }
+    }
+
+    //push to github, then continue with TOP
 
 
-
-    
     
